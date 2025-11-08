48746e1ac3c3f06e8d37359cbc5bba2afa663200
Ashok326-del <mamillapalliashok2468@gmail.com>
2025-11-08 16:28:56 +0530

    code added to java file

--- app.java
+++ app.java
@@ -1,1 +1,10 @@
 This is java file
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;   // first number
+        int num2 = 20;   // second number
+        int sum = num1 + num2;  // adding the numbers
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
